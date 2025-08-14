01df3a927fcd3fcf788669ab0248f5c46aee4255
TopWrite <write@topthink.com>
2025-08-14 11:17:27 +0800

    release

---
+++ asset/plugins/ai/570-bf3c3f.js
@@ -0,0 +1,12 @@
+"use strict";(self.webpackChunkTopWritePlugins_ai=self.webpackChunkTopWritePlugins_ai||[]).push([[570],{6570:(e,r,t)=>{t.r(r),t.d(r,{default:()=>a});t(8194),t(75),t(9963),t(3708),t(6746),t(2208),t(1022),t(6877),t(2793),t(2990),t(2674);var n=t(3183),o=t.n(n),c=t(9460),i=t.n(c),p=t(3036),u=["uiSchema"];function f(e,r){var t=Object.keys(e);if(Object.getOwnPropertySymbols){var n=Object.getOwnPropertySymbols(e);r&&(n=n.filter((function(r){return Object.getOwnPropertyDescriptor(e,r).enumerable}))),t.push.apply(t,n)}return t}function s(e){for(var r=1;r<arguments.length;r++){var t=null!=arguments[r]?arguments[r]:{};r%2?f(Object(t),!0).forEach((function(r){o()(e,r,t[r])})):Object.getOwnPropertyDescriptors?Object.defineProperties(e,Object.getOwnPropertyDescriptors(t)):f(Object(t)).forEach((function(r){Object.defineProperty(e,r,Object.getOwnPropertyDescriptor(t,r))}))}return e}function a(e){var r=e.Component,t=e.props,n=t.uiSchema,o=i()(t,u);return(0,p.jsx)(r,s(s({},o),{},{uiSchema:s(s({},n),{},{endpoint:{"ui:widget":"hidden"}})}))}},9161:(e,r,t)=>{
+/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+var n=t(9196),o=Symbol.for("react.element"),c=Symbol.for("react.fragment"),i=Object.prototype.hasOwnProperty,p=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,u={key:!0,ref:!0,__self:!0,__source:!0};function f(e,r,t){var n,c={},f=null,s=null;for(n in void 0!==t&&(f=""+t),void 0!==r.key&&(f=""+r.key),void 0!==r.ref&&(s=r.ref),r)i.call(r,n)&&!u.hasOwnProperty(n)&&(c[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===c[n]&&(c[n]=r[n]);return{$$typeof:o,type:e,key:f,ref:s,props:c,_owner:p.current}}r.Fragment=c,r.jsx=f,r.jsxs=f},3036:(e,r,t)=>{e.exports=t(9161)}}]);
+//# sourceMappingURL=570-bf3c3f.js.map
